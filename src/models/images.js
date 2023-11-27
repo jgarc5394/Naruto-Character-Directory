f4a79bc05c008a147f356ac67dafac4fa2d2b786
Julio Garcia <jgarc5394@gmail.com>
2023-11-26 18:44:43 -0600

    Removed unnecessary endpoints

--- src/models/images.js
+++
@@ -1,8 +0,0 @@
-import db from '../utils/db'
-
-export const getImages = async () => db.image.findMany()
-
-export const getImage = async (id) =>
-  db.image.findUnique({ where: { imageId: id } })
-
-export const addImage = async (name) => db.image.create({ data: { name } })
--- src/models/jutsus.js
+++
@@ -1,8 +0,0 @@
-import db from '../utils/db'
-
-export const getJutsus = async () => db.jutsu.findMany()
-
-export const getJutsu = async (id) =>
-  db.jutsu.findUnique({ where: { jutsuId: id } })
-
-export const addJutsu = async (name) => db.jutsu.create({ data: { name } })
