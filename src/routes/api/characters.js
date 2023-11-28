7ccb87d4d9ec9f538c400465957f92e990dc633a
Julio Garcia <jgarc5394@gmail.com>
2023-11-27 20:39:07 -0600

    Fixed /api/characters.js with else statements

--- src/routes/api/characters.js
+++ src/routes/api/characters.js
@@ -19,32 +19,36 @@ router.get('/:id', (req, res) => {
   const character = getCharacter(req.params.id)
   if (character) {
     res.send(character)
+  } else {
+    res.status(404).send({ msg: 'Character not found' })
   }
-  res.status(404).send({ msg: 'Character not found' })
 })
 
 router.post('/', (req, res) => {
   const newCharacter = createCharacter(req.body)
   if (newCharacter) {
     res.status(201).send(newCharacter)
+  } else {
+    res.status(400).send({ msg: 'Bad request' })
   }
-  res.status(400).send({ msg: 'Bad request' })
 })
 
 router.put('/:id', (req, res) => {
   const updatedCharacter = updateCharacter(req.params.id, req.body)
   if (updatedCharacter) {
     res.send(updatedCharacter)
+  } else {
+    res.status(404).send({ msg: 'Character not found' })
   }
-  res.status(404).send({ msg: 'Character not found' })
 })
 
 router.delete('/:id', (req, res) => {
   const deleted = deleteCharacter(req.params.id)
   if (deleted) {
     res.send({ msg: `Character ${req.params.id} Deleted` })
+  } else {
+    res.status(404).send({ msg: 'Character not found' })
   }
-  res.status(404).send({ msg: 'Character not found' })
 })
 
 export default router
